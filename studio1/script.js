0b8b9a09460b66c3f5fe79c735113e18b5883c1f
dnmacedo <dnmacedo@ucdavis.edu>
2017-01-16 23:49:32 -0800

    simplified js

--- studio1/script.js
+++ studio1/script.js
@@ -1,6 +1,3 @@
-var w=700;
-var h=300;
-var frameCnt=0;
 var timeAccel=1.0;
 var increment=1.0;
 
@@ -16,7 +13,6 @@ function setup() {
 
 
 function draw() {
-  frameCnt=frameCnt + 1;
 
   fill(0);
   ellipseMode(CENTER);
@@ -26,15 +22,11 @@ function draw() {
      if(abs(mouseX-pmouseX)>10)
      {
      stroke(abs(mouseX-2*pmouseX),abs(mouseY-2*pmouseY),0);
-     increment=increment*1.01;
-     timeAccel=timeAccel+increment;
      ellipse(mouseX,mouseY,50,50);
   }
     else
   {
      stroke(abs(mouseX-2*pmouseX),0,abs(mouseY-2*pmouseY));
-     increment=increment*1.01;
-     timeAccel=timeAccel+increment;
      ellipse(mouseX,mouseY,50,50);
   }
 }
