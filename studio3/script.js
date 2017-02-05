74fececd25d3bfe756744c338f9bb1ad32fedbff
dnmacedo <dnmacedo@ucdavis.edu>
2017-02-04 19:32:05 -0800

    studio 3

---
+++ studio3/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+
+var cat = document.getElementById('cat');
+var info = document.getElementById('info');
+var catInfo =document.getElementById('catInfo');
+var close = document.getElementById('close');
+
+cat.addEventListener('click', function()
+{
+  info.style.display="block";
+  catInfo.style.display="block";
+})
+
+cat.addEventListener('mouseover', function()
+{
+  cat.style.transition = "filter 1s";
+});
+
+cat.addEventListener('mouseout', function()
+{
+  cat.style.transition = "filter 1s";
+})
+
+close.addEventListener('click', function()
+{
+  info.style.display="none";
+})
+
+});
