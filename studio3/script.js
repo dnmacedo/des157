8ecb44017aab4ce51d570b8337577d4e71fc53c6
dnmacedo <dnmacedo@cruess207-21.ucdavis.edu>
2017-02-05 18:00:57 -0800

    adding photoshopped images

--- studio3/script.js
+++ studio3/script.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event){
   console.log("DOM fully loaded and parsed");
 
+// declare all variables
 var cat = document.getElementById('cat');
+var record = document.getElementById('record');
 var info = document.getElementById('info');
 var catInfo =document.getElementById('catInfo');
 var close = document.getElementById('close');
 
-cat.addEventListener('click', function()
-{
-  info.style.display="block";
-  catInfo.style.display="block";
-})
-
+// make all the functions for hovering effects
 cat.addEventListener('mouseover', function()
 {
   cat.style.transition = "filter .5s";
@@ -19,12 +16,40 @@ cat.addEventListener('mouseover', function()
 
 cat.addEventListener('mouseout', function()
 {
-  cat.style.transition = "filter 2s";
+  cat.style.transition = "filter .5s";
 })
+
+record.addEventListener('mouseover', function()
+{
+  record.style.transition = "filter .5s";
+})
+
+record.addEventListener('mouseout', function()
+{
+  record.style.transition = "filter .5s";
+})
+
+// make all the functions for the pop up info
+
+cat.addEventListener('click', function()
+{
+  info.style.display = "block";
+  catInfo.style.display = "block";
+})
+
+record.addEventListener('click', function()
+{
+  info.style.display = "block";
+  recordInfo.style.display = "block";
+})
+
+// close function
 
 close.addEventListener('click', function()
 {
   info.style.display="none";
+  catInfo.style.display = "none";
+  recordInfo.style.display = "none";
 })
 
 });
