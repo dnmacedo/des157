30eef245b4d89ca57518ed576c74e7f2decf40a5
dnmacedo <dnmacedo@ucdavis.edu>
2017-02-06 01:45:07 -0800

    fixing images to size

--- studio3/script.js
+++ studio3/script.js
@@ -7,23 +7,28 @@ var img = document.getElementsByTagName('img');
 // declare all png variables
 var cat = document.getElementById('cat');
 var record = document.getElementById('record');
-
+var ring = document.getElementById('ring');
+var tamb = document.getElementById('tamb');
+var shirt = document.getElementById('shirt');
 //declare all pop up window variables
 var info = document.getElementById('info');
 var catInfo =document.getElementById('catInfo');
 var recordInfo = document.getElementById('recordInfo');
+var ringInfo = document.getElementById('ringInfo');
+var tambInfo = document.getElementById('tambInfo');
+var shirtInfo = document.getElementById('shirtInfo');
 // delcare close variable
 var close = document.getElementById('close');
 
 // make all the functions for hovering effects
-img.addEventListener('mouseover', function()
+cat.addEventListener('mouseover', function()
 {
-  img.style.transition = "filter .5s";
+  cat.style.transition = "filter .5s";
 });
 
-img.addEventListener('mouseout', function()
+cat.addEventListener('mouseout', function()
 {
-  img.style.transition = "filter .5s";
+  cat.style.transition = "filter .5s";
 })
 
 // record.addEventListener('mouseover', function()
@@ -50,6 +55,24 @@ record.addEventListener('click', function()
   recordInfo.style.display = "block";
 })
 
+ring.addEventListener('click', function()
+{
+  info.style.display = "block";
+  ringInfo.style.display = "block";
+})
+
+tamb.addEventListener('click', function()
+{
+  info.style.display = "block";
+  tambInfo.style.display = "block";
+})
+
+shirt.addEventListener('click', function()
+{
+  info.style.display = "block";
+  shirtInfo.style.display = "block";
+})
+
 // close function
 
 close.addEventListener('click', function()
@@ -57,6 +80,9 @@ close.addEventListener('click', function()
   info.style.display="none";
   catInfo.style.display = "none";
   recordInfo.style.display = "none";
+  ringInfo.style.display = "none";
+  tambInfo.style.display = "none";
+  shirt.style.display = "none";
 })
 
 });
