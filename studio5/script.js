344e9e70b722eedfe02b2d33118f6954733375ae
dnmacedo <dnmacedo@ucdavis.edu>
2017-02-20 22:37:19 -0800

    studio5

---
+++ studio5/script.js
@@ -0,0 +1,41 @@
+// velocity.js
+
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+
+var q = document.getElementById("question");
+var rightA = document.getElementById("rightA");
+var wrongA = document.getElementById("wrongA");
+
+
+
+rightA.addEventListener("click", function(){
+
+  for(var i = 0; i < 2; i++){
+    Velocity("rightA",
+      { y: "+=200" }
+    );
+
+    Velocity("rightA",
+      "reverse"
+    );
+
+    console.log("rightA bounced");
+  }
+
+});
+
+wrongA.addEventListener("click", function(){
+
+    Velocity("wrongA",
+    { x: "+=100" });
+    Velocity("wrongA",
+    { x: "-=200" });
+    Velocity("wrongA",
+    "reverse");
+    console.log("rightA bounced");
+
+
+});
+
+});
