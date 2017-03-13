4885c185565061fcdb0ba6744e5f4b01c309ecbd
dnmacedo <dnmacedo@ucdavis.edu>
2017-03-12 22:53:36 -0700

    proto 3

---
+++ project/process/proto3/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(event){
+  console.log("DOM fully loaded and parsed");
+
+var myMsg = document.getElementById("myMsg");
+
+
+alert("Welcome! Please change the screen size to 'iPhone6' in the toggle device button to view this interface. (right click, inspect, phone/tablet symbol).")
+
+
+
+
+
+
+});
