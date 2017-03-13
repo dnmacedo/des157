4306ee5e56bfb1ed3a927179b6a2b1ce32345571
dnmacedo <dnmacedo@ucdavis.edu>
2017-03-13 14:16:18 -0700

    proto3

--- project/process/proto3/script.js
+++ project/process/proto3/script.js
@@ -2,11 +2,70 @@ document.addEventListener("DOMContentLoaded", function(event){
   console.log("DOM fully loaded and parsed");
 
 var myMsg = document.getElementById("myMsg");
+var story1 = document.getElementById("story1");
+var story2 = document.getElementById("story2");
+var story3 = document.getElementById("story3");
+var story4 = document.getElementById("story4");
+var story5 = document.getElementById("story5");
+var story6 = document.getElementById("story6");
+
+var storytitle = document.getElementById("storytitle");
+var storyblurb = document.getElementById("storyblurb");
+
+if (window.width >= 450){
+  alert("Welcome! Please change the screen size to 'iPhone6' in the toggle device button to view this interface. (right click, inspect, phone/tablet symbol).");
+}
+
+// start read.html interactions
+
+story1.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
+
+});
+
+story2.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
+
+});
+
+story3.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
+
+});
+
+story4.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
+
+});
+
+story5.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
+
+});
+
+story6.addEventListener("clicked", function(){
+
+  storytitle.innerHTML = "John starts the day...";
+
+  storyblurb.innerHTML = "John is an aspiring actor. On his way to his audition for a big movie, he missed his bus! He thought, “No big deal, I’ve got plenty of time.” He decided to walk instead. Walking down the street he noticed a woman bent over picking up her belongings from the ground. John said hello and offered to help, but the woman said, “NO!” This was unexpected, but nonetheless, John happily continued walking. He saw a cat down an alley that soon followed him all the way to his destination.";
 
 
-alert("Welcome! Please change the screen size to 'iPhone6' in the toggle device button to view this interface. (right click, inspect, phone/tablet symbol).")
-
-
+});
 
 
 
